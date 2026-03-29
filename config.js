68e5ec7698d2f9b57260578edc0f74ccfc33177f
Filip <tanic.filip.97@gmail.com>
2026-03-29 18:02:19 +0200

    Fixing URI

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Frontend configuration — edit this file to change settings
-window.API_BASE_URL = 'https://qr-code-storage-backend-macee5uqg-tanicfilips-projects.vercel.app/'; // leave empty for localhost:3000;
+window.API_BASE_URL = 'https://qr-code-storage-backend.vercel.app'; // leave empty for localhost:3000;
